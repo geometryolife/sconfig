94a1979117966177fd2dd96b2edc59ce34769f77
Joe <1716975442@qq.com>
2020-08-07 21:55:28 +0800

    added centos7_packages.sh

---
+++ centos7_packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+yum -y install git
+yum -y install zsh
+yum -y install gcc
+yum -y install curl
+yum -y install wget
+yum -y install python3
+yum -y install make
+yum -y install cmake
+yum -y install autoconf
+yum -y install automake
+yum -y install pkgconfig
+yum -y install libevent-devel
+yum -y install ncurses-devel
+yum -y install bison
+yum -y install ninja-build
+yum -y install libtool
+yum -y install gcc-c++
+yum -y install unzip
+yum -y install patch
