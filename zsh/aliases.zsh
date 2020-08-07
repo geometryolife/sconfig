7f5197c1026a23377510c0787e58fdd0a7c727c4
Joe <1716975442@qq.com>
2020-08-07 10:39:16 +0800

    first commit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,18 @@
+alias l='ls -la'
+alias c='clear'
+alias n='neofetch'
+alias ra='ranger'
+
+# Editor
+alias vi='nvim'
+
+# Tmux
+alias t='tmux'
+alias tl='tmux ls'
+alias tn='tmux new -s'
+alias tt='tmux attach'
+alias ta='tmux attach -t'
+alias td='tmux detach'
+alias tk='tmux kill-session -t'
+alias ts='tmux switch -t'
+alias tr='tmux rename-session -t'
