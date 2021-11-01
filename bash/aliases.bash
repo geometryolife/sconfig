96f101cd69d90fa9efc9ddf46bc2aebf366305f7
Joe Chen <geometryolife@gmail.com>
2021-11-02 01:39:43 +0800

    feat(bash, .gitignore, lazygit, pip, ranger): add
    bash/aliases.bash: add alias
    gitconf/goplus.cfg: include configuration
    lazygit/config.yml: create
    pip/pip.conf: add source
    ranger/rc.conf: update for lazygit

---
+++ bash/aliases.bash
@@ -0,0 +1,9 @@
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias vi='nvim'
+alias ra='ranger'
+alias lg='lazygit'
+alias ep='export http_proxy=http://127.0.0.1:7890 && export https_proxy=https://127.0.0.1:7890 && export socks5_proxy=socks5://127.0.0.1:7891'
+alias up='unset http_proxy && unset https_proxy && unset socks5_proxy'
